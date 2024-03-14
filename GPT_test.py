4de03bcc65b7effabe4ff4334210b926f673d0bc
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-03-14 18:06:30 -0400

    Update GPT_test.py

--- GPT_test.py
+++ GPT_test.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-data = st.file_uploader("Upload your input CSV file", type=['csv'])
-
 st.set_page_config(page_title= f"SQR Dash",page_icon="🧑‍🚀",layout="wide")
+
+data = st.file_uploader("Upload your input CSV file", type=['csv'])
                    
 if data is not None:
     # Assuming the CSV has headers, otherwise use header=None
