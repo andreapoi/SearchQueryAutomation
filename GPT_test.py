59e1bdaffaa51a5007eb57e350871582c3863ff5
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-03-14 17:59:43 -0400

    Update GPT_test.py

--- GPT_test.py
+++ GPT_test.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 data = st.file_uploader("Please upload Search Query Terms", type = [csv])
 
-st.set_page_config(page_title= f"SQR Dash",page_icon="🧑‍🚀",layout="wide"
+st.set_page_config(page_title= f"SQR Dash",page_icon="🧑‍🚀",layout="wide")
                    
 if 'data' not in st.session_state:
     # Assuming the CSV has headers, otherwise use header=None
